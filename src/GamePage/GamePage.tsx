ab8813e5a90a1ef616d822eac92360a4dbbefd81
Levi Liester <levi.liester@gmail.com>
2024-01-06 13:16:33 -0600

    Show questions and answers
    Don't worry about style, thats secondary to functionality

--- src/GamePage/GamePage.tsx
+++ src/GamePage/GamePage.tsx
@@ -1,42 +1,22 @@
 import { useEffect, useState } from "react";
-import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, Divider, Flex, Text } from "@chakra-ui/react";
 import { getGameById } from "../api/games";
 import "./GamePage.css";
 import { useParams } from "react-router-dom";
 import formatDate from "../utils/dates";
 
-const ScoreBoard = () => {
+const ScoreBoard = ({ players }: { players: any[] }) => {
   return (
     <Flex
       justifyContent={"space-around"}
       alignItems={"center"}
-      borderBottom={"3px solid #4299e1"}
+      borderBottom={"3px solid"}
+      borderBottomColor={"blue.400"}
       p={3}
     >
-      <Flex direction={"column"} alignItems={"center"}>
-        <Text fontSize={20} fontWeight={"500"}>
-          Home
-        </Text>
-        <Text fontSize={20} fontWeight={"500"}>
-          20
-        </Text>
-      </Flex>
-      <Flex direction={"column"} alignItems={"center"} mx={3}>
-        <Text fontSize={20} fontWeight={"500"}>
-          Time
-        </Text>
-        <Text fontSize={20} fontWeight={"500"}>
-          12:00
-        </Text>
-      </Flex>
-      <Flex direction={"column"} alignItems={"center"}>
-        <Text fontSize={20} fontWeight={"500"}>
-          Away
-        </Text>
-        <Text fontSize={20} fontWeight={"500"}>
-          0
-        </Text>
-      </Flex>
+      {players.map((player) => {
+        return <Avatar name={player.name} />;
+      })}
     </Flex>
   );
 };
@@ -52,15 +32,24 @@ const GamePage = () => {
   const startDate = new Date(game?.startDate);
   return (
     <Flex direction={"column"}>
-      <ScoreBoard />
+      <ScoreBoard players={game?.players || []} />
       <Flex direction={"column"}>
         <Text>Title: {game?.title}</Text>
         <Text>Starts: {formatDate(startDate)}</Text>
-        <Text>Current Players: {game?.currentPlayerCount}</Text>
-        <Box>Questions</Box>
+        <Text mb={5}>Current Players: {game?.currentPlayerCount}</Text>
+        <Divider height={"4px"} backgroundColor={"blue.400"} />
+        <Box fontSize={"xl"} textAlign={"center"}>
+          Questions
+        </Box>
         {game?.questions.map((question: any) => (
           <Box key={question.id} mb={3}>
             <Text>{question.text}</Text>
+            {question.answers.map((answer: any, index: number) => (
+              <Flex key={answer.id}>
+                <Text>{`${index + 1}: ${answer.text}`}</Text>
+              </Flex>
+            ))}
+            <Divider height={"4px"} backgroundColor={"blue.400"} />
           </Box>
         ))}
       </Flex>
