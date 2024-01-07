9cabd21f998544b2f7e6fd5274d3d5170016c1e9
Levi Liester <levi.liester@gmail.com>
2024-01-07 15:01:21 -0600

    Make it so you can submit an answer to a question
    No use feedback that it was submitted buuuuut thats where we are

--- src/GamePage/GamePage.tsx
+++ src/GamePage/GamePage.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { Component, useEffect, useState } from "react";
 import { Avatar, Box, Button, Divider, Flex, Text } from "@chakra-ui/react";
-import { getGameById } from "../api/games";
+import GamesAPI from "../api/games";
 import "./GamePage.css";
 import { useParams } from "react-router-dom";
 import formatDate from "../utils/dates";
@@ -20,15 +20,50 @@ const ScoreBoard = ({ players }: { players: any[] }) => {
     </Flex>
   );
 };
+
+const Question = ({ question }: { question: any }) => {
+  const [selectedAnswerId, setSelectedAnswerId] = useState<string>("");
+  return (
+    <Box mb={10}>
+      <Text>{question.text}</Text>
+      {question?.answers.map((answer: any, index: number) => {
+        return (
+          <Flex
+            key={answer._id}
+            onClick={() => setSelectedAnswerId(answer._id)}
+          >
+            <Text>
+              {`${index + 1}: ${answer.text}`}
+              {selectedAnswerId === answer._id && "✅"}
+            </Text>
+          </Flex>
+        );
+      })}
+      <Button
+        onClick={() => {
+          GamesAPI.submitAnswer({
+            questionId: question._id,
+            gameId: question._id,
+            answerId: selectedAnswerId,
+          });
+        }}
+      >
+        Submit
+      </Button>
+    </Box>
+  );
+};
 const GamePage = () => {
   const [game, setGame] = useState<any>(null);
   const { gameId } = useParams();
   useEffect(() => {
     if (!gameId) return;
-    getGameById({ gameId }).then(({ data }) => {
+    GamesAPI.getGameById({ gameId }).then(({ data }) => {
       setGame(data);
     });
   }, []);
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  if (!game) return null;
   const startDate = new Date(game?.startDate);
   return (
     <Flex direction={"column"}>
@@ -42,15 +77,7 @@ const GamePage = () => {
           Questions
         </Box>
         {game?.questions.map((question: any) => (
-          <Box key={question.id} mb={3}>
-            <Text>{question.text}</Text>
-            {question.answers.map((answer: any, index: number) => (
-              <Flex key={answer.id}>
-                <Text>{`${index + 1}: ${answer.text}`}</Text>
-              </Flex>
-            ))}
-            <Divider height={"4px"} backgroundColor={"blue.400"} />
-          </Box>
+          <Question key={question.id} question={question} />
         ))}
       </Flex>
     </Flex>
--- src/api/games.ts
+++ src/api/games.ts
@@ -24,12 +24,34 @@ export const updateGame = (gameId: string, eventUpdate: IGameUpdate) => {
 
 export const createGame = (newEvent: IGameCreate) => {
   return axios.post(`/api/games/create`, newEvent);
-}
+};
 
-export const getGameById = ({gameId}:{gameId: string}) => {
+export const getGameById = ({ gameId }: { gameId: string }) => {
   return axios.get(`/api/games/${gameId}`);
 };
 
 export const getPublicGames = () => {
   return axios.get(`/public/games`);
+};
+
+export const submitAnswer = ({
+  gameId,
+  questionId,
+  answerId,
+}: {
+  gameId: string;
+  questionId: string;
+  answerId: string;
+}) => {
+  return axios.post(`/api/games/${gameId}/question/${questionId}/answer`, {
+    answerId,
+  });
+};
+
+export default {
+  updateGame,
+  createGame,
+  getGameById,
+  getPublicGames,
+  submitAnswer,
 };
