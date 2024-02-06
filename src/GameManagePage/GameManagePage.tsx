b04c09711fa3db5c93c39e4e3d8777d6127d1a15
skylersidner <malevolentdragon@gmail.com>
2024-02-05 21:51:08 -0600

    Adding ability to add questions w/ multiple answers; WIP
    needs way to mark type (single/multi) and isCorrect value

--- src/GameManagePage/GameManagePage.tsx
+++ src/GameManagePage/GameManagePage.tsx
@@ -103,8 +103,8 @@ const GameManagePage = () => {
   }, []);
 
   const [isAddingQuestion, setIsAddingQuestion] = useState<any>(false);
+  const [addedAnswers, setAddedAnswers] = useState<any>([]);
   const [questionText, setQuestionText] = useState<string>("");
-  const [answer1Text, setAnswer1Text] = useState<string>("");
 
   function getCurrentGame(gameId: string) {
     gamesService.getGameById({ gameId }).then(({ data }) => {
@@ -113,14 +113,14 @@ const GameManagePage = () => {
   }
 
   function addQuestion(): void {
+    setAddedAnswers([{ text: "", isCorrect: false }]);
+    setIsAddingQuestion(true);
+  }
+
+  function createQuestion(): void {
     const question = {
       text: questionText,
-      answers: [
-        {
-          text: answer1Text,
-          isCorrect: true,
-        },
-      ],
+      answers: addedAnswers,
     };
     console.log(question);
     gamesService.addQuestions({ gameId, questions: [question] }).then(() => {
@@ -128,7 +128,25 @@ const GameManagePage = () => {
     });
     setIsAddingQuestion(false);
     setQuestionText("");
-    setAnswer1Text("");
+  }
+
+  function addAnswer(): void {
+    setAddedAnswers([...addedAnswers, { text: "", isCorrect: false }]);
+  }
+
+  function removeAnswer(event: any): void {
+    const index = parseInt(event.target.dataset.index, 10);
+    setAddedAnswers((addedAnswers: any) => {
+      return structuredClone(addedAnswers).splice(index, 1);
+    });
+  }
+
+  function setAddedAnswerText(text: string, index: number): void {
+    setAddedAnswers((addedAnswers: any) => {
+      const newAddedAnswers = structuredClone(addedAnswers);
+      newAddedAnswers[index].text = text;
+      return newAddedAnswers;
+    });
   }
 
   return (
@@ -144,7 +162,12 @@ const GameManagePage = () => {
       })}
       {game?.questions?.map((question: any) => {
         return (
-          <Question question={question} gameId={gameId!} setGame={setGame} />
+          <Question
+            key={question._id}
+            question={question}
+            gameId={gameId!}
+            setGame={setGame}
+          />
         );
       })}
       <Flex>
@@ -153,10 +176,10 @@ const GameManagePage = () => {
             ml={2}
             onClick={(event) => {
               event.stopPropagation();
-              setIsAddingQuestion(true);
+              addQuestion();
             }}
           >
-            Add Question
+            Add Another Question
           </Button>
         )}
 
@@ -175,21 +198,35 @@ const GameManagePage = () => {
                 />
               </InputGroup>
             </FormControl>
+            {addedAnswers.map((answer: IAnswer, idx: number) => (
+              <FormControl isRequired key={idx}>
+                <FormLabel>Answer {idx + 1}</FormLabel>
+                <InputGroup>
+                  <Textarea
+                    value={answer.text}
+                    name={"answer" + (idx + 1) + "-text"}
+                    resize="vertical"
+                    id={"add-question-form-answer-" + (idx + 1) + "-text"}
+                    placeholder={"Answer " + (idx + 1)}
+                    onChange={(e) => setAddedAnswerText(e.target.value, idx)}
+                  />
+                </InputGroup>
 
-            <FormControl isRequired>
-              <FormLabel>Answer 1</FormLabel>
-              <InputGroup>
-                <Textarea
-                  value={answer1Text}
-                  name="answer1-text"
-                  resize="vertical"
-                  id={"add-question-form-answer-1-text"}
-                  placeholder="Yellow... no green!  Aaaaaah...!"
-                  onChange={(e) => setAnswer1Text(e.target.value)}
-                />
-              </InputGroup>
-            </FormControl>
-
+                <Button
+                  colorScheme="blue"
+                  bg="blue.400"
+                  color="white"
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  isLoading={false}
+                  disabled={false}
+                  onClick={(e) => removeAnswer(e)}
+                >
+                  Remove Answer
+                </Button>
+              </FormControl>
+            ))}
             <Button
               colorScheme="blue"
               bg="blue.400"
@@ -199,9 +236,22 @@ const GameManagePage = () => {
               }}
               isLoading={false}
               disabled={false}
-              onClick={(event) => addQuestion()}
+              onClick={(event) => addAnswer()}
             >
-              Add Question
+              Add Another Answer
+            </Button>
+            <Button
+              colorScheme="blue"
+              bg="blue.400"
+              color="white"
+              _hover={{
+                bg: "blue.500",
+              }}
+              isLoading={false}
+              disabled={false}
+              onClick={(event) => createQuestion()}
+            >
+              Create Question
             </Button>
           </VStack>
         )}
